87c418d9c3b0f2017833430c0ebf066d600c1526
ardiantrik <ardiantrik@gmail.com>
2020-05-09 16:37:56 +0700

    fix upload image base64

--- beta/app.py
+++ beta/app.py
@@ -6,6 +6,7 @@ from PIL import Image
 from io import BytesIO
 from werkzeug.utils import secure_filename
 from datetime import datetime
+from stegano import lsb
 
 encode_folder = "Encoded_image_" + str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 UPLOAD_FOLDER = os.getcwd() + '/assets/'
@@ -24,8 +25,15 @@ def go_upload(file_up):
         filename = secure_filename(file_up.filename)
         file_up.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     
+    return filename
+
+def process_encode(cover_img, hidden_text):
+    secret = lsb.hide(UPLOAD_FOLDER + "/" + cover_img, hidden_text)
+    secret.save(UPLOAD_FOLDER + "/" + encode_folder + "/LSB-" + cover_img)
     return 'success'
-    
+
+def process_decode(stego_img):
+    return 'decode process'
 
 #=====Route Section=====
 @app.route('/')
@@ -48,23 +56,34 @@ def show_decode():
 def show_identify():
     return render_template('hal_identify.html')
 
-# @app.route('/image2matrix', methods=['POST'])
-# def image2matrix():
-#     return "haha"
-
 @app.route('/go_encode', methods=['GET', 'POST'])
 def go_encode():
     # check if the post request has the file part
     if request.method == 'POST' and 'ori_image' in request.files:
         cover_img = request.files['ori_image']
         if request.form['action'] == 'Encode Pesan' and request.form['hidden_text'] != '' and cover_img.filename != '':
-            go_upload(cover_img)
+            new_cover_img = go_upload(cover_img)
             hidden_text = request.form['hidden_text']
-            return hidden_text
+            if process_encode(new_cover_img,hidden_text) == 'success':
+                return 'iso'
+            else:
+                return render_template('hal_error.html')
+            
         elif request.form['action'] == 'Encode Gambar' and 'hidden_image' in request.files and cover_img.filename != '':
             hidden_img = request.files['hidden_image']
-            go_upload(cover_img)
-            go_upload(hidden_img)
+            new_cover_img = go_upload(cover_img)
+            new_hidden_img = go_upload(hidden_img)
+            #img = Image.open(UPLOAD_FOLDER+"/"+hidden_img.filename)
+            f = open(UPLOAD_FOLDER+"/"+new_hidden_img, 'rb')
+            img = f.read()
+            b64 = str(base64.b64encode(img))
+            b64 = b64.split("'")
+            b64_hidden = 'data:image;base64, '+b64[1]
+            if process_encode(new_cover_img,b64_hidden) == 'success':
+                return '<img src="'+b64_hidden+'" />'
+            else:
+                return render_template('hal_error.html')
+
             
         else:
             return render_template('hal_error.html')
